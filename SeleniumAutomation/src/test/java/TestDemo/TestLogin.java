f96906bae592a370b6442b47729978c5f58dc985
QSG <QSG@192.168.1.35>
2022-02-01 12:50:10 +0530

    First Commit

---
+++ SeleniumAutomation/src/test/java/TestDemo/TestLogin.java
@@ -0,0 +1,42 @@
+package TestDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestLogin {
+	
+	WebDriver driver=null;
+	
+	@BeforeTest
+	public void BeforeSetUp()
+	{
+		WebDriverManager.edgedriver().setup();
+		driver=new EdgeDriver();
+	}
+
+	@Test
+	public void testLogin() throws InterruptedException {
+
+		driver.get("https://www.saucedemo.com/");
+		//driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.findElement(By.id("user-name")).sendKeys("DemoUser");
+		driver.findElement(By.id("password")).sendKeys("password");
+		driver.findElement(By.id("login-button")).click();
+	}
+	
+	@AfterTest
+	public void AfterMethod()
+	{
+		driver.close();
+		driver.quit();
+		System.out.println("Test Executed successfully");
+	}
+
+}
+ 
