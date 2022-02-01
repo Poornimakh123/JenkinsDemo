b3e5b08f11b1ae9e1fef8e9f5782f0e4dda84665
QSG <QSG@DESKTOP-LOTIA2H>
2022-02-01 15:05:42 +0530

    First commit

--- SeleniumAutomation/src/test/java/TestDemo/TestLogin.java
+++ SeleniumAutomation/src/test/java/TestDemo/TestLogin.java
@@ -1,14 +1,11 @@
 package TestDemo;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class TestLogin {
 
 	WebDriver driver = null;
